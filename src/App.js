d4f3514291dd2b6ffb341689371f56ef7c541b6a
Claud10matheus <claudiom.pst@gmail.com>
2023-09-07 16:09:50 -0300

    instalando e entendendo o react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 
 function App() {
 
-  const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />
- 
   return (
-    pagina
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Inicio /> } />
+        <Route path="/sobremim" element={ <SobreMim /> } />
+        <Route path="*" element={<h1> Página não foi encontrada! ;-; </h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
