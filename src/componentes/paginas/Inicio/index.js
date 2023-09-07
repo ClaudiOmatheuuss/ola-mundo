887b8549ad72b1192cdbc534bcc5571936ccadc6
Claud10matheus <claudiom.pst@gmail.com>
2023-09-07 19:47:14 -0300

    criando banner e configurando absolute import

--- src/componentes/paginas/Inicio/index.js
+++ src/componentes/paginas/Inicio/index.js
@@ -1,5 +1,11 @@
+import Banner from "componentes/Banner";
+
 export default function Inicio () {
     return (
-        <h1>Olá Mundo</h1>
+        <main>
+            <Banner />
+            
+            <h1>Olá Mundo</h1>
+        </main>
     )
 }
--- src/componentes/paginas/SobreMim/index.js
+++ src/componentes/paginas/SobreMim/index.js
@@ -1,5 +1,11 @@
+import Banner from "componentes/Banner";
+
 export default function SobreMim () {
     return (
-        <h1>Sobre Mim . . . </h1>
+        <main>
+            <Banner />
+            
+            <h1>Sobre Mim . . . </h1>
+        </main>
     )
 }
